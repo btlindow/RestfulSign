43cf484aa60a7c2cd78cbf544dcbd1de7ced6315
Benjamin Lindow <benjamin.lindow@swri.org>
2018-07-19 12:56:17 -0500

    Added VolleySingleton Class. Added Volley Post Call in MainActivity.

--- app/src/main/java/com/example/blindow/nathansboard/MainActivity.java
+++ app/src/main/java/com/example/blindow/nathansboard/MainActivity.java
@@ -2,18 +2,17 @@ package com.example.blindow.nathansboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,40 +32,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ackTV.setText("Message Pushed");
-                sendRequest(messageET.getText().toString());
+                postMessage(messageET.getText().toString());
             }
         });
     }
 
-    public void sendRequest(final String message) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://httpbin.org/post";
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>()
-                {
+    private void postMessage(final String message) {
+        String url = "ENTER_URL_HERE";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // response
-                        Log.d("NHB", response);
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
                     }
                 },
-                new Response.ErrorListener()
-                {
+                new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // error
-                        Log.d("NHB", error.toString());
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-                }
-        ) {
+                }) {
             @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String>  params = new HashMap<String, String>();
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
                 params.put("message", message);
                 return params;
             }
         };
-        queue.add(postRequest);
+        VolleySingleton.getInstance(MainActivity.this).addToRequestQueue(stringRequest);
     }
 }
